3831da2df3cacf2d112d2ed022c3273e17b002d0
aziz <108581396+lewin007@users.noreply.github.com>
2024-08-28 19:49:04 +0100

    ff

--- script.js
+++ script.js
@@ -3,99 +3,98 @@ document.addEventListener("DOMContentLoaded", function() {
     const submitBtn = document.getElementById('submit-btn');
     const selectedStickersLabel = document.getElementById('selected-stickers-label');
 
+// Categories and their corresponding image counts
+const stickerCategories = {
+    'basketball': 6,
+    'one-piece': 20, // Use lowercase and hyphenated format
+    'meme': 20,
+    "arcane":2,
+    "architecture":5,
+    "art":9,
+    "attackOnTitins":2,
+    "avatar":3,
+    "avenger":9,
+    "batman":2,
+    "berserk":5,
+    "blackClover":2,
+    "bleatch":4,
+    "box":1,
+    "fightClub":2,
+    "breakingBad":3,
+    "classroomOfElits":2,
+    "dark":1,
+    "deadpool":4,
+    "deathnote":3,
+    "demonSlayer":2,
+    "dragonBall":2,
+    "fightClub":2,
+    "animefilms":2,
+    "football":10,
+    "forestGump":1,
+    "freinds":2,
+    "games":4,
+    "godFather":1,
+    "gta":5,
+    "gumbull":2,
+    "harryPotter":6,
+    "howIMetYourMother":1,
+    "howToTrainYourDragon":1,
+    "hxh":1,
+    "info":10,
+    "intersteller":1,
+    "invincible":2,
+    "jjk":34,
+    "jojo":1,
+    "joker":2,
+    "klem":27,
+    "kongfuPanda":1,
+    "lacasadelpaper":1,
+    "lalaland":1,
+    "love":5,
+    "loveIsWar":1,
+    "matrix":1,
+    "miyamora":1,
+    "monsters":1,
+    "moonNight":1,
+    "mrRobot":1,
+    "mushukutensai":1,
+    "myHeroAcademy":1,
+    "naruto":4,
+    "nimo":1,
+    "nurse":10,
+    "oiretsOfKaribian":1,
+    "onePenchman":1,
+    "openhaimer":1,
+    "palastine":5,
+    "peakyBlinders":7,
+    "prisonBreak":2,
+    "rapanzul":2,
+    "ratatoille":1,
+    "rickAndMorty":7,
+    "science":2,
+    "sialentVoice":2,
+    "simson":1,
+    "soloLeveling":4,
+    "songsAndSingers":6,
+    "spacetoon":10,
+    "spiderman":2,
+    "spongbob":5,
+    "strangerThings":1,
+    "the100":1,
+    "topGun":1,
+    "toyStory":1,
+    "villandSaga":5,
+    "wednesday":1,
+    "zatla":3,
 
-    // Categories and their corresponding image counts
-    const stickerCategories = {
-        'basketball': 6,
-        'one-piece': 20, // Use lowercase and hyphenated format
-        'meme': 20,
-        "arcane":2,
-        "architecture":5,
-        "art":9,
-        "attackOnTitins":2,
-        "avatar":3,
-        "avenger":9,
-        "batman":2,
-        "berserk":5,
-        "blackClover":2,
-        "bleatch":4,
-        "box":1,
-        "fightClub":2,
-        "breakingBad":3,
-        "classroomOfElits":2,
-        "dark":1,
-        "deadpool":4,
-        "deathnote":3,
-        "demonSlayer":2,
-        "dragonBall":2,
-        "fightClub":2,
-        "animefilms":2,
-        "football":10,
-        "forestGump":1,
-        "freinds":2,
-        "games":4,
-        "godFather":1,
-        "gta":5,
-        "gumbull":2,
-        "harryPotter":6,
-        "howIMetYourMother":1,
-        "howToTrainYourDragon":1,
-        "hxh":1,
-        "info":10,
-        "intersteller":1,
-        "invincible":2,
-        "jjk":34,
-        "jojo":1,
-        "joker":2,
-        "klem":27,
-        "kongfuPanda":1,
-        "lacasadelpaper":1,
-        "lalaland":1,
-        "love":5,
-        "loveIsWar":1,
-        "matrix":1,
-        "miyamora":1,
-        "monsters":1,
-        "moonNight":1,
-        "mrRobot":1,
-        "mushukutensai":1,
-        "myHeroAcademy":1,
-        "naruto":4,
-        "nimo":1,
-        "nurse":10,
-        "oiretsOfKaribian":1,
-        "onePenchman":1,
-        "openhaimer":1,
-        "palastine":5,
-        "peakyBlinders":7,
-        "prisonBreak":2,
-        "rapanzul":2,
-        "ratatoille":1,
-        "rickAndMorty":7,
-        "science":2,
-        "sialentVoice":2,
-        "simson":1,
-        "soloLeveling":4,
-        "songsAndSingers":6,
-        "spacetoon":10,
-        "spiderman":2,
-        "spongbob":5,
-        "strangerThings":1,
-        "the100":1,
-        "topGun":1,
-        "toyStory":1,
-        "villandSaga":5,
-        "wednesday":1,
-        "zatla":3,
-
-        // Add other categories and the number of images for each
-    };
+    // Add other categories and the number of images for each
+};
 
     Object.keys(stickerCategories).forEach(category => {
         const imageCount = stickerCategories[category];
         for (let i = 1; i <= imageCount; i++) {
             const img = document.createElement('img');
-            img.src = `${category}/${category} (${i}).JPG`;  // Load image based on folder and name
+            img.src = `${category}/${category} (${i}).jpg`;  // Load image based on folder and name
             img.alt = `${category} Sticker ${i}`;
             img.classList.add('sticker'); // Add class to image
 
@@ -116,26 +115,25 @@ document.addEventListener("DOMContentLoaded", function() {
             thumbnailsContainer.appendChild(stickerDiv);
         }
     });
-});
+
     // Add event listener to submit button
-submitBtn.addEventListener('click', function() {
-    const selectedStickers = [];
-    const checkboxes = document.querySelectorAll('.thumbnails input[type="checkbox"]:checked');
+    submitBtn.addEventListener('click', function() {
+        const selectedStickers = [];
+        const checkboxes = document.querySelectorAll('.thumbnails input[type="checkbox"]:checked');
 
-    checkboxes.forEach(checkbox => {
-        const category = checkbox.classList[0];
-        const index = checkbox.value.match(/\d+$/)[0];  // Extract number from value
-        selectedStickers.push(`${category}/${category} (${index})`);
+        checkboxes.forEach(checkbox => {
+            const category = checkbox.classList[0];
+            const index = checkbox.value.match(/\d+$/)[0];  // Extract number from value
+            selectedStickers.push(`${category}/${category} (${index})`);
+        });
+
+        if (selectedStickers.length > 0) {
+            selectedStickersLabel.textContent = selectedStickers.join(', ');
+        } else {
+            selectedStickersLabel.textContent = 'No stickers selected';
+        }
     });
-
-    if (selectedStickers.length > 0) {
-        selectedStickersLabel.textContent = selectedStickers.join(', ');
-    } else {
-        selectedStickersLabel.textContent = 'No stickers selected';
-    }
 });
-
-
 
 // Filter stickers based on category
 function filterStickers() {
@@ -158,16 +156,15 @@ function filterStickers() {
 function unselectAllStickers() {
     const checkboxes = document.querySelectorAll('.thumbnails input[type="checkbox"]');
     checkboxes.forEach(checkbox => checkbox.checked = false);
-        // Clear the label
-        const selectedStickersLabel = document.getElementById('selected-stickers-label');
-        selectedStickersLabel.textContent = ''; // Clear the label text
+
+    // Clear the label
+    const selectedStickersLabel = document.getElementById('selected-stickers-label');
+    selectedStickersLabel.textContent = ''; // Clear the label text
 }
-
-
 
 document.addEventListener("scroll", function() {
     const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-    if (window.scrollY > 300) {
+    if (window.scrollY > 600) {
         scrollToTopBtn.classList.add("visible");
     } else {
         scrollToTopBtn.classList.remove("visible");
@@ -181,3 +178,19 @@ document.getElementById("scrollToTopBtn").addEventListener("click", function() {
     });
 });
 
+document.addEventListener("scroll", function() {
+    const scrollTodownBtn = document.getElementById("scrollTodownBtn");
+    if (window.scrollY > 600) {
+        scrollTodownBtn.classList.add("visible");
+    } else {
+        scrollTodownBtn.classList.remove("visible");
+    }
+});
+
+document.getElementById("scrollTodownBtn").addEventListener("click", function() {
+    const scrollAmount = window.innerHeight*57; // You can adjust this value as needed
+    window.scrollTo({
+        top: window.scrollY + scrollAmount,
+        behavior: "smooth"
+    });
+});
